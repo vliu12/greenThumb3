a1a1b8690e1bbe67aab8a61f8b58c46bc5f299ee
victoria <liuvictoria12@gmail.com>
2024-09-14 10:45:54 -0400

    button testing

--- pages/js/activity.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("myButton");
-    button.addEventListener("click", function() {
-        button.textContent = "you clicked me!";
-    });
-});
